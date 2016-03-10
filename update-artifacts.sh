b01828fa2457a817281b089610ab0248320d5306
Timothy Hunter <timhunter@databricks.com>
2016-03-10 08:22:40 -0800

    using maven instead

---
+++ update-artifacts.sh
@@ -0,0 +1,12 @@
+GROUPID="org.tensorframes"
+VERSION="0.0.1-0.7.1"
+
+for file in "javacpp" "tensorflow-linux-x86_64" "tensorflow"; do
+	mvn install:install-file \
+	 -DgroupId=$GROUPID \
+	 -DartifactId=$file \
+	 -Dversion=$VERSION \
+	 -Dpackaging=jar \
+	 -Dfile=binaries/$file.jar \
+	 -DlocalRepositoryPath=deploy/
+done
